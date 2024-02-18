d22155cb81bb06192293d9cecdafc2ac1e58eb9d
QuiseB29 <bmarquise544@gmail.com>
2024-02-17 18:56:00 -0600

    multi line poem

---
+++ poem2.py
@@ -0,0 +1,10 @@
+"""
+Python, in the realm of code you shine.
+With simplicity and grace, you're truly divine. 
+Try to Study, learn new ways. 
+Things will 
+come easy, 
+you'll see. Keep focus, 
+and soon to be free """
+print("Python, in the realm of code you shine. With simplicity and grace, you're truly divine.")
+print("Try to Study, learn new ways. Things will come easy, you'll see. Keep focus and soon to be free")
